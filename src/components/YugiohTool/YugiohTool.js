a7dff5f754b875cfafaced6398c9bf10b22ca5e0
chris <104324755+cjurgens17@users.noreply.github.com>
2023-10-03 11:01:23 -0400

    Fixed some styling bugs. Parent grid now expands with children, card decks now have a fixed size in relation to viewport and dont shrink. Cards are being rendered properly when dropped, and auto rows are working. Next issue will be to solve getting the scrollcardView to be fixed positioning

--- src/components/YugiohTool/YugiohTool.js
+++ src/components/YugiohTool/YugiohTool.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import CardScrollView from "../CardScrollView/CardScrollView";
 import CardResults from "../CardResults";
@@ -6,47 +6,58 @@ import SortCards from "../SortCards/SortCards";
 import OpenFilterModal from "../OpenFilterModal/OpenFilerModal";
 import FilterModal from "../FilterModal/FilterModal";
 // import Carousel from "../Carousel";
-import DeckName from '../DeckName';
+import DeckName from "../DeckName";
 
-import styles from './YugiohTool.module.css';
-import MainDeck from '../MainDeck';
-import SideDeck from '../SideDeck';
-import ExtraDeck from '../ExtraDeck';
+import styles from "./YugiohTool.module.css";
+import MainDeck from "../MainDeck";
+import SideDeck from "../SideDeck";
+import ExtraDeck from "../ExtraDeck";
 
+function YugiohTool() {
+  const [filterModal, setFilterModal] = React.useState(false);
+  const [showFiltered, setShowFiltered] = React.useState(false);
+  const [filteredCards, setFilteredCards] = React.useState([]);
 
-function YugiohTool(){
-    const [filterModal, setFilterModal] = React.useState(false);
-    const [showFiltered, setShowFiltered] = React.useState(false);
-    const [filteredCards, setFilteredCards] = React.useState([]);
-
-    return (
-        <div className={styles.gridWrapper}>
-        {filterModal && (
-          <FilterModal setShowFiltered={setShowFiltered} setFilterModal={setFilterModal} filteredCards={filteredCards} setFilteredCards={setFilteredCards}></FilterModal>
-        )}
-        <header className={styles.filter}>
-          <DeckName/>
-        </header>
-        <main className={styles.details}>
-          {/* <Carousel></Carousel> */}
-          <MainDeck/>
-          <SideDeck/>
+  return (
+    <div className={styles.gridWrapper}>
+      {filterModal && (
+        <FilterModal
+          setShowFiltered={setShowFiltered}
+          setFilterModal={setFilterModal}
+          filteredCards={filteredCards}
+          setFilteredCards={setFilteredCards}
+        ></FilterModal>
+      )}
+      <header className={styles.filter}>
+        <DeckName />
+      </header>
+      <main className={styles.details}>
+        {/* <Carousel></Carousel> */}
+          <MainDeck />
+          <SideDeck />
           <ExtraDeck/>
-        </main>
-        <aside>
-          <section className={styles.header}>
-            <CardResults showFiltered={showFiltered} filteredCards={filteredCards}/>
-            <OpenFilterModal setFilterModal={setFilterModal}/>
-          </section>
-          <section className={styles.sort}>
-            <SortCards/>
-          </section>
-          <section className={styles.cards}>
-          <CardScrollView showFiltered={showFiltered} filteredCards={filteredCards}/>
-          </section>
-        </aside>
-      </div>
-    );
+      </main>
+      <aside className={styles.cardScroll}>
+        
+        <section className={styles.header}>
+          <CardResults
+            showFiltered={showFiltered}
+            filteredCards={filteredCards}
+          />
+          <OpenFilterModal setFilterModal={setFilterModal} />
+        </section>
+        <section className={styles.sort}>
+          <SortCards />
+        </section>
+        <section className={styles.cards}>
+          <CardScrollView
+            showFiltered={showFiltered}
+            filteredCards={filteredCards}
+          />
+        </section>
+      </aside>
+    </div>
+  );
 }
 
 export default YugiohTool;
