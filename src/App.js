42da2138fff2e5929ae763865c64d8585d6a1af7
chris <104324755+cjurgens17@users.noreply.github.com>
2023-10-05 11:16:44 -0400

    Added minimal header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import YuGiOhCardProvider from "./components/YuGiOhCardsContext/YuGiOhCardsContext";
 import React from "react";
 import YugiohTool from "./components/YugiohTool";
+import Header from './components/Header';
 import './App.css';
 
 function App() {
 
   return (
     <YuGiOhCardProvider>
+      <header className="header">
+        <Header/>
+        </header>
      <div className="wrapper">
         <YugiohTool/>
         </div>
